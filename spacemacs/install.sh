f71abda06419b0c4222aaa3f77b8dbb279cec587
Kyle McKean <mckean.kylej@gmail.com>
2016-08-02 19:14:37 -0400

    antigen now lives under zsh as zsh depends on it

--- spacemacs/install.sh
+++ spacemacs/install.sh
@@ -1,5 +1,5 @@
 set -e
 echo "Installing Spacemacs"
-git clone https://github.com/syl20bnr/spacemacs.git "$HOME/.emacs.d"
-git clone https://github.com/cydparser/spacemacs-intero.git "$HOME/.emacs.d/private/intero"
+[ ! -d "$HOME/.emacs.d" ] && git clone https://github.com/syl20bnr/spacemacs.git "$HOME/.emacs.d"
+[ ! -d "$HOME/.emacs.d/private/intero" ] && git clone https://github.com/cydparser/spacemacs-intero.git "$HOME/.emacs.d/private/intero"
 echo "Spacemacs installed"
